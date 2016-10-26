5dad646c05af5056be1ce6965253686976a327d6
adamdahan <adam@88labs.com>
2016-10-26 14:56:47 -0400

    Adding new read me image files / assets folder

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    lazy var thnxViewController: ThnxViewController = {
+    // Vanilla view controller
+    lazy var vanillaThnxViewController: ThnxViewController = {
         let urls = [Url.Alamofire,
                     Url.MBProgressHUD,
                     Url.ImagePicker,
@@ -22,6 +23,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     Url.Algorithm,
                     Url.Material]
         return ThnxViewController(urls: urls)
+    }()
+    
+    // Custom view controller
+    lazy var customThnxViewController: ThnxViewController = {
+        let urls = [Url.Alamofire,
+                    Url.MBProgressHUD,
+                    Url.ImagePicker,
+                    Url.Graph,
+                    Url.Algorithm,
+                    Url.Material]
+        
+        let t = ThnxViewController(urls: urls)
+        t.title = "Open Source Libraries"
+        t.sectionHeaderFont = UIFont.boldSystemFont(ofSize: 24) // Cancels UITableView section header Dynamic Text Sizing
+        t.sectionHeaderTextColor = UIColor.white
+        t.sectionHeaderBackgroundColor = UIColor.blue
+        
+        t.contentViewBackgroundColor = UIColor.blue.withAlphaComponent(0.6)
+        
+        t.textLabelFont = UIFont.italicSystemFont(ofSize: 10) // Cancels UITableViewCell textLabel Dynamic Text Sizing
+        t.textLabelTextColor = UIColor.white
+        t.textLabelBackgroundColor = UIColor.clear
+        t.textLabelLayerBorderColor = UIColor.clear.cgColor
+        t.textLabelLayerCornerRadius = 0
+        return t
     }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -34,7 +60,7 @@ extension AppDelegate {
 
     func prepareWindow() {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: thnxViewController)
+        window?.rootViewController = UINavigationController(rootViewController: vanillaThnxViewController) // customThnxViewController
         window?.makeKeyAndVisible()
     }
 }
